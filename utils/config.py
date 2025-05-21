c649cb42b3d803e1d9dcd495d7a127e5567d6ab2
syarotokki <maikuratokki@gmail.com>
2025-05-21 10:57:58 +0900

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,18 +1,17 @@
 import json
 import os
 
-CONFIG_FILE = "config.json"
+CONFIG_FILE = 'config.json'
 
 def load_config():
     if not os.path.exists(CONFIG_FILE):
         return {}
     try:
-        with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+        with open(CONFIG_FILE, 'r') as f:
             return json.load(f)
     except json.JSONDecodeError:
         return {}
 
 def save_config(config):
-    with open(CONFIG_FILE, "w", encoding="utf-8") as f:
-        json.dump(config, f, indent=4, ensure_ascii=False)
-
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(config, f, indent=4)
