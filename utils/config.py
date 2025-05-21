f61d495b8602fb865afbc1aa00ecad0c8f4fa38e
syarotokki <maikuratokki@gmail.com>
2025-05-21 11:17:18 +0900

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -15,3 +15,15 @@ def load_config():
 def save_config(config):
     with open(CONFIG_FILE, 'w') as f:
         json.dump(config, f, indent=4)
+
+def get_config(guild_id):
+    config = load_config()
+    return config.get(str(guild_id))
+
+def set_config(guild_id, data):
+    config = load_config()
+    config[str(guild_id)] = data
+    save_config(config)
+
+def get_log_channel():
+    return None
